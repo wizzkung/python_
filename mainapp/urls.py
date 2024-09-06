12ff3a4495d057ee68f7b0f73aac12373e593c74
itstart <itstart@213-5>
2024-09-06 21:33:19 +0500

    lesson_01 hws

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from mainapp.views import index, contacts
+from mainapp.views import index, contacts, about, product, products
 
 urlpatterns = [
     path('', index),
     path('contacts/', contacts),
+    path('about/', about),
+    path('product/', product),
+    path('products/', products)
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -7,3 +7,12 @@ def index(request):
 
 def contacts(request):
     return render(request, 'contacts.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def product(request):
+    return render(request,'product.html')
+
+def products(request):
+    return render(request,'products.html')
